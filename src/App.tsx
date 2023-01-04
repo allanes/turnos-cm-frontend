e06d31048b0a80eedcbed6624e2df766813c09ce
GAldereteHero <herogonzalo@gmail.com>
2023-01-04 17:15:32 -0300

    Se incluye sidebar con su navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header/Header';
 import { OfficesContainer } from './components/OfficesContainer/OfficesContainer';
 import { PatientContainer } from './components/PatientContainer/PatientContainer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SideBar } from './components/SideBar/SideBar';
+import { DoctorsContainer } from './components/DoctorsContainer/DoctorsContainer';
+import { PatientsContainer } from './components/PatientsContainer/PatientsContainer';
+import { TurnsContainer } from './components/TurnsContainer/TurnsContainer';
+import { categoriesList_sidebar } from './services/categoriesList_sidebar';
 
 function App() {
 
   return (
-    <div className='mi-app'>
+    <>
       <Header />
-      {/* <PatientContainer /> */}
-      <OfficesContainer />
-    </div>
+      <div className='mi-app'>
+        <Router>
+          <SideBar />
+          <Routes>
+            <Route path='/' element={<OfficesContainer />} />
+            <Route path={categoriesList_sidebar[1].link} element={<DoctorsContainer />} />
+            <Route path={categoriesList_sidebar[2].link} element={<PatientsContainer />} />
+            <Route path={categoriesList_sidebar[3].link} element={<TurnsContainer />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/services/categoriesList_sidebar.ts
@@ -0,0 +1,6 @@
+export const  categoriesList_sidebar = [
+    {title: "Consultorios", link: "/", icon:''},
+    {title: "Médicos", link: "/doctors", icon:''},
+    {title: "Pacientes", link: "/patients", icon:''},
+    {title: "Turnos", link: "/turns", icon:''}
+]
