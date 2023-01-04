6084c64ea7cb598292f69ecb3bddcfee554e7f77
GAldereteHero <herogonzalo@gmail.com>
2023-01-04 15:37:42 -0300

    Se integra servicios de consultorios

--- src/components/OfficesContainer/OfficesContainer.tsx
+++ src/components/OfficesContainer/OfficesContainer.tsx
@@ -1,25 +1,26 @@
 import { useEffect, useState } from 'react'
 import { OfficesDescription } from './OfficesDescription'
 
-import { ConsultorioDetallado } from '../../codegen_output'
+import { ConsultorioDetallado, ConsultoriosService } from '../../codegen_output'
 
 interface OfficesState {
   offices: Array<ConsultorioDetallado>
 }
+
+// const consultoriosServices = new ConsultoriosService()
 
 export const OfficesContainer = () => {
 
   const [officesList, setOfficesList] = useState<OfficesState["offices"]>([])
 
   useEffect(() => {
-    fetch('http://localhost:8000/api/v1/offices/with-details?skip=0&limit=100')
-      .then(res => res.json())
-      .then(offices => {
-        console.log(offices);
-        setOfficesList(offices)
-      })
+    ConsultoriosService.readConsultoriosConDetallesApiV1OfficesWithDetailsGet()
+    .then(offices => {
+      console.log(offices);
+      setOfficesList(offices)
+    })
   }, [])
-
+  
   return (
     <>
       <OfficesDescription officesList={officesList} />
---
+++ src/services/patientList_initial_states.tsx
@@ -0,0 +1,14 @@
+export const patientList_initial_states = [
+    {
+      dni: 36866647,
+      patientName: 'Gonzalo',
+      patientSurName: 'Alderete',
+      email: 'herogonzalo@gmail.com'
+    },
+    {
+      dni: 34568985,
+      patientName: 'Adrian',
+      patientSurName: 'Llanes',
+      email: 'adrianllanes@gmail.com'
+    }
+]
