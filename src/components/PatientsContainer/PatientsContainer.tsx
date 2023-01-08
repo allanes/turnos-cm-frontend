2e94feb8a8afbb055e566fa163623f471c1a3386
GAldereteHero <herogonzalo@gmail.com>
2023-01-07 22:49:02 -0300

    Se agrega lista de  pacientes con la func delete

--- src/components/PatientsContainer/PatientsContainer.tsx
+++ src/components/PatientsContainer/PatientsContainer.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Paciente } from '../../codegen_output'
+import { PacientesService } from '../../codegen_output'
+import { PatientsList } from './PatientsList'
+
+interface PatientsState {
+  patients: Array<Paciente>
+}
 
 export const PatientsContainer = () => {
+
+  const [patientsList, setPatientsList] = useState<PatientsState["patients"]>([])
+
+  useEffect(() => {
+    PacientesService.readPacientesApiV1PatientsGet()
+    .then(patients => {
+      console.log(patients);
+      setPatientsList(patients)
+    })
+  }, [])
+
+  const handleDelete = (id: number): void => {
+    console.log(id);
+    PacientesService.deletePacienteApiV1PatientsIdDelete(id)
+    setPatientsList(patientsList.filter((patient) => patient.id !== id));
+  }
+  
   return (
-    <div>PatientsContainer</div>
+    <div>
+      <PatientsList patientsList={patientsList} onDeletePatient={handleDelete} />
+    </div>
   )
 }
