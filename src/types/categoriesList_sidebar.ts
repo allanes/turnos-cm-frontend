8b88d92d04b79b017227a7b5f340209b1d5369d2
GAldereteHero <herogonzalo@gmail.com>
2023-01-13 14:59:14 -0300

    Se divide las vistas y genera la de pacientes

--- src/types/categoriesList_sidebar.ts
+++ src/types/categoriesList_sidebar.ts
@@ -5,7 +5,7 @@ export const  categoriesList_sidebar = [
     {title: "Turnos", link: "/turns", icon:''},
     {title: "Sala de espera", link: "/waitingRoom/0", icon:''},
     {title: "Sala de espera 1", link: "/waitingRoom/1", icon:''},
-    {title: "Sala de espera 2", link: "/waitingRoom/2", icon:''}
+    {title: "Sala de espera 2", link: "/waitingRoom/2", icon:''},
 ]
 
 export const url_waitingRoom = '/waitingRoom'
