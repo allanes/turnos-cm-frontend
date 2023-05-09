9de52ecfe50235af4ddba260cad77bb28eab4345
Adrian Llanes <ados.adrian@gmail.com>
2023-05-09 03:18:45 -0300

    agregando carousel con bootstrap

--- src/components/Views/PatientsView/PatientViewOfficeDetail.tsx
+++ src/components/Views/PatientsView/PatientViewOfficeDetail.tsx
@@ -25,49 +25,62 @@ export const handleRefresh = (
 };
 
 export const PatientViewOfficeDetail = ({ officesList, consultorioId, animationActive }: OfficesState) => {
-  
+  const itemsToShow = 3;
+  const totalSlides = Math.ceil(officesList.length / itemsToShow);
 
   return (
     <div>
       <div className="container-fluid text-center">
         <div className="row align-items-center">
-          <div className="cardRoom-container">
-            {officesList.map((office) => {
-              const numPatientsLeft = (office.pacientes?.length ?? 0) - 3;
-              return (
-                <div key={office.id} className={`cardRoom${animationActive && office.id === consultorioId ? " animation-active" : ""}`}>
-                  <div className='cardRoom-Top'>
-                    <p className='h1'>Dr. {office.medico}</p>
-                    <p>Consultorio: {office.id}</p>
+          <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+            <div className="carousel-inner">
+              {[...Array(totalSlides)].map((_, slideIndex) => {
+                const isActive = slideIndex === 0; // Make the first slide active
+                return (
+                  <div key={slideIndex} className={`carousel-item${isActive ? ' active' : ''}`}>
+                    {[...Array(itemsToShow)].map((_, cardIndex) => {
+                      const officeIndex = (slideIndex * itemsToShow + cardIndex) % officesList.length;
+                      const office = officesList[officeIndex];
+  
+                      const numPatientsLeft = (office.pacientes?.length ?? 0) - 3;
+                      return (
+                        <div key={office.id} className={`cardRoom${animationActive && office.id === consultorioId ? " animation-active" : ""}`}>
+                          <div className='cardRoom-Top'>
+                            <p className='h1'>Dr. {office.medico}</p>
+                            <p>Consultorio: {office.id}</p>
+                          </div>
+                          <div>
+                            <table className='table table-striped table-hover table-xxl table-container-sm table-turns-container'>
+                              <tbody>
+                                {office.pacientes?.slice(0, 3).map((turn, index) => {
+                                  const isFirstPatient = index === 0;
+                                  return (
+                                    <tr key={index} className={isFirstPatient ? 'table-warning' : ''}>
+                                      <th scope='row'><p className='mt-2'>{index + 1}</p></th>
+                                      <td className='h2 text-start'>{turn}</td>
+                                    </tr>
+                                  );
+                                })}
+                                {office.pacientes?.length && office.pacientes.length > 3 &&
+                                  <tr>
+                                    <td colSpan={3} style={{ textAlign: 'center', fontSize: '1.2rem', fontWeight: 'bold', color: '#808080' }}>
+                                      + {(office.pacientes?.length ?? 0) - 3} pacientes
+                                    </td>
+                                  </tr>
+                                }
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
-                  <div>
-                    <table className='table table-striped table-hover table-xxl table-container-sm table-turns-container'>
-                      <tbody>
-                        {office.pacientes?.slice(0,3).map((turn, index) => {
-                          const isFirstPatient = index === 0;
-                          return (
-                            <tr key={index} className={isFirstPatient ? 'table-warning' : ''}>
-                              <th scope='row'><p className='mt-2'>{index + 1}</p></th>
-                              <td className='h2 text-start'>{turn}</td>
-                            </tr>
-                          )
-                      })}
-                        {office.pacientes?.length && office.pacientes.length > 3 &&
-                          <tr>
-                            <td colSpan={3} style={{textAlign: 'center', fontSize: '1.2rem', fontWeight: 'bold', color: '#808080'}}>
-                              + {(office.pacientes?.length ?? 0) - 3} pacientes
-                            </td>
-                          </tr>
-                        }
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              )
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/styles.scss'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
