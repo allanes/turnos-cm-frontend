a61873a7f995381473f54fd706b7f083a412420d
Adrian Llanes <ados.adrian@gmail.com>
2023-05-18 03:21:39 -0300

    agregado reproductor de videos bajados

--- src/components/Views/PatientsView/PatientView.tsx
+++ src/components/Views/PatientsView/PatientView.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { PatientViewListContainer } from './PatientViewListContainer'
 import { PatientViewOfficeList } from './PatientViewOfficeList'
+// import YouTubePlayer from 'react-player/youtube'
+// import GoogleDrivePlayer from './GoogleDrivePlayer';
+import VideoPlayer from './VideoPlayer';
+
 
 export const PatientView = () => {
   return (
@@ -10,7 +14,12 @@ export const PatientView = () => {
         <div className='pages-container'>
           <Routes>
             <Route path="/patientsView" element={<PatientViewListContainer />} />
-            <Route path="/patientsView/:roomId" element={<PatientViewOfficeList />} />
+            <Route path="/patientsView/:roomId" element={
+              <div style={{display: 'flex', width: '100%'}}>
+                <PatientViewOfficeList />
+                <VideoPlayer />
+              </div>
+            } />
           </Routes>
         </div>
       </Router>
