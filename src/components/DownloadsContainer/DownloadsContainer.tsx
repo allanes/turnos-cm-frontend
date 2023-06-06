2c817246ef703f7c39b701e0f7930d310b594994
Adrian Llanes <ados.adrian@gmail.com>
2023-06-06 01:06:45 -0300

    Agregados botones de descarga para iniciar y apagar teles

--- src/components/DownloadsContainer/DownloadsContainer.tsx
+++ src/components/DownloadsContainer/DownloadsContainer.tsx
@@ -1,34 +1,88 @@
 import React from 'react'
 import qrIcon from '../../assets/icons/qr-code-scan.svg'
+import pdfIcon from '../../assets/icons/filetype-pdf.svg'
 import { QrService } from '../../codegen_output/services/QrService';
 
 export const DownloadsContainer = () => {
+
+  const handleDownloadPdf = async (response: Response, nombre_descarga: string) => {
+    try {
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', nombre_descarga); // or any other filename you want
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+          console.error('An error occurred while fetching the QR code:', error);
+      }
+  }
   
-    const downloadQR = async () => {
-        try {
-            const response = await fetch('/descargar_qr', {
-                method: 'GET',
-                headers: {
-                'Accept': 'application/pdf',
-                },
-            });
-            const blob = await response.blob();
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'qr_medicos.pdf'); // or any other filename you want
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-        } catch (error) {
-            console.error('An error occurred while fetching the QR code:', error);
-        }
+  const downloadQR = async () => {
+      try {
+          const response = await fetch('/descargar_qr', {
+              method: 'GET',
+              headers: {
+              'Accept': 'application/pdf',
+              },
+          });
+          await handleDownloadPdf(response, "qr_medicos.pdf");
+      } catch (error) {
+          console.error('An error occurred while fetching the QR code:', error);
+      }
   };
 
+  const downloadManualIniciarTeles = async () => {
+    try {
+        const response = await fetch('/descargar_manual_iniciar_teles', {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/pdf',
+            },
+        });
+        handleDownloadPdf(response, "manual_iniciar_teles.pdf");
+    } catch (error) {
+        console.error('An error occurred while fetching the manual:', error);
+    }
+  }
+
+  const downloadManualApagarTeles = async () => {
+    try {
+        const response = await fetch('/descargar_manual_apagar_teles', {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/pdf',
+            },
+        });
+        handleDownloadPdf(response, "manual_apagar_teles.pdf");
+    } catch (error) {
+        console.error('An error occurred while fetching the manual:', error);
+    }
+  }
+
   return (
-    <button className="btn btn-primary" onClick={downloadQR}>
-      <img src={qrIcon} alt="QR Icon" style={{ width: '20px', marginRight: '10px' }} />
-      Descargar QR para médicos
-    </button>
+    <div>
+
+      <div>
+        <button className="btn btn-outline-primary" onClick={downloadQR}>
+          <img src={qrIcon} alt="QR Icon" style={{ width: '20px', marginRight: '10px' }} />
+          QR para médicos
+        </button>
+      </div>
+      <div>
+        <button className="btn btn-outline-primary" onClick={downloadManualIniciarTeles}>
+          <img src={pdfIcon} alt="QR Icon" style={{ width: '20px', marginRight: '10px' }} />
+          Manual para Iniciar Teles
+        </button>
+      </div>
+      <div>
+        <button className="btn btn-outline-primary" onClick={downloadManualApagarTeles}>
+          <img src={pdfIcon} alt="QR Icon" style={{ width: '20px', marginRight: '10px' }} />
+          Manual para Apagar Teles
+        </button>
+      </div>
+    </div>
   );
 };
